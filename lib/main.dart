cc1b6a0529358ac6f2b2a55875da6415a073eed6
sourav <sauravrawat710@gmail.com>
2023-10-03 19:17:46 +0530

    section 6 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:place_finder_mvvm/home_page.dart';
+import 'package:place_finder_mvvm/view_models/place_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: ChangeNotifierProvider(
+        create: (context) => PlaceListViewModel(),
+        child: const HomePage(),
+      ),
     );
   }
 }
